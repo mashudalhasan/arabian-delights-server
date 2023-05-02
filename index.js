17ae07a15c1424ff747d54a0e53f277a75752314
mashudalhasan <mashudalhasan554@gmail.com>
2023-05-02 15:30:07 +0600

    added chefsInfo data file

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 
+const chefsInfo = require("./data/chefsInfo.json");
+
 app.get("/", (req, res) => {
   res.send("Chef is hunting");
+});
+
+app.get("/chefsInfo", (req, res) => {
+  res.send(chefsInfo);
 });
 
 app.listen(port, () => {
